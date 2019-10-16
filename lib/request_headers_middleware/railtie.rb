df047fc15e955471e9f349c44be24bf72be943cf
batizhevskii <batizhevskii@fidor.de>
2019-10-16 17:18:07 +0200

    Rails 5 support

--- lib/request_headers_middleware/railtie.rb
+++ lib/request_headers_middleware/railtie.rb
@@ -7,11 +7,15 @@ module RequestHeadersMiddleware
     config.request_headers_middleware.blacklist = []
     config.request_headers_middleware.callbacks = []
 
-    initializer 'request_headers_middleware.insert_middleware' do
-      config.app_middleware.insert_after ActionDispatch::RequestId,
-                                         RequestHeadersMiddleware::Middleware
-      config.after_initialize do
-        RequestHeadersMiddleware.setup(config.request_headers_middleware)
+    initializer 'request_headers_middleware.insert_middleware' do |app|
+      if ActionDispatch.const_defined? :RequestId
+        app.config.middleware.insert_after ActionDispatch::RequestId, RequestHeadersMiddleware::Middleware
+      else
+        app.config.middleware.insert_after Rack::MethodOverride, RequestHeadersMiddleware::Middleware
+      end
+
+      app.config.after_initialize do
+        RequestHeadersMiddleware.setup(app.config.request_headers_middleware)
       end
     end
   end
